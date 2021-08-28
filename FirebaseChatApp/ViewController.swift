8b6e4adabb3a5448d26ebfb2b477f1d1ab77faaa
Suraj Kumar <surajk.if@gmail.com>
2021-08-28 10:22:34 +0530

    Initial Commit for in App messaging.

---
+++ FirebaseChatApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FirebaseChatApp
+//
+//  Created by Suraj Singh on 27/08/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
